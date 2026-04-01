6580389e1ad821b8f5a09758282ccaffe172d02e
DOUGLAS MENEGUETTI <dodomeneguetti@gmail.com>
2026-03-31 21:41:28 -0300

    segundo commit

--- checkpoint1.py
+++ checkpoint1.py
@@ -7,3 +7,9 @@ tentativas = 0
 limite = 7
 acertou = False
 
+print("🎯 Tente adivinhar o número entre 1 e 100!")
+print(f"Você tem {limite} tentativas.\n")
+
+while tentativas < limite:
+    palpite = int(input("Digite seu palpite: "))
+    tentativas += 1
